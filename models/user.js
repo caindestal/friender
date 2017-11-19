02f5f13aeb152f3a7913af990a43891cf7b63404
Orlando J Galicia <orlandoj.galicia@gmail.com>
2017-11-19 11:22:06 -0400

    Modicando modelo user, acorde a documentación

--- models/user.js
+++ models/user.js
@@ -9,19 +9,27 @@ const Schema = mongoose.Schema
   message: 'Las Contraseñas no son iguales'
 }
 */
+
+//Colección user moongose
+
 const UserSchema = new Schema({
-  firt_name: String, // Los 2 o un nombre del usuario
-  last_name: String, // Los 2 o un apellido del usuario
-  username: {type: 'String', maxlength: [50, 'Your username is extensive'], minlength: [3, 'Your username is short']}, // nombre de usuario con el que el se registrar
-  password: {type: 'String', minlength: [7, 'Password must be greater than 7 characters']}, //   contraseña con la cual el usuario se registrara
-  place_birth: String,  // lugar de nacimiento del usuario
-  place_residence: String, // lugar de residencia del usuario
-  birthdate: { type: Date, required: 'birthdate is obligatory' }, // Fecha de nacimiento del usuario es obligatoria
-  email: { type: 'String', unique: true, lowercase: true, required: 'Email is obligatory' }, // correo con el usuario se registro total mente obligario y tiene que ser unico
-  tel: {type: Number, unique: true, required: 'Number Celphone is obligatory'}, // telefono obligatorio del cliente
-  tel_Opc: Number, // telefono opcional del usuarios
-  permision_level: {type: Number, default: 1}, // Crea un nivel de permiso al usuario para saber asi cuanto es el poder que tiene este en la app
-  singUpDate: {type: Date, default: Date.now()} // el dia que se unio al sistema
+  name: String, // Nombres
+  last_name: String, // Apellidos
+  idcc: {type: Number, unique: true, required: 'CC is obligatory'} // Cedula de identidad                           
+  place_birth: String,  // lugar de nacimiento
+  email: { type: 'String', unique: true, lowercase: true, required: 'Email is obligatory' }, // correo, campo unico y requerido.
+  tel: {type: Number, unique: true, required: 'Number Celphone is obligatory'}, // telefono, campo requerido
+  username: {type: 'String', maxlength: [50, 'Your username is extensive'], minlength: [4, 'Your username is short']}, // nombre de logeo
+  nickname: {type: 'String', maxlength: [20, 'Your nickname is extensive'], minlength: [4, 'Your nickname is short']}, // nombre de perfil
+  password: {type: 'String', minlength: [7, 'Password must be greater than 7 characters']}, // contraseña del usuario, minimo 8 caracteres
+  birthdate: { type: Date, required: 'birthdate is obligatory' }, // Fecha de nacimiento, campo requerido
+  sex: { type: String, enum: ['male', 'female']}, // tipo de sexo
+  place_residence: String, // lugar de residencia 
+  profile_img: String, // imagen de perfíl
+  banner: String, //imagen de portada
+  tel_home: Number, // Teléfono fijo 
+  permision_level: {type: Number, default: 1}, // Nivel autorización de usuario
+  singUpDate: {type: Date, default: Date.now()} // Fech registro
 })
 
-module.exports = mongoose.model('User', UserSchema)// crea un export para el modelo con User y su esquema a la base de datos
+module.exports = mongoose.model('User', UserSchema)// se exporta coleccion User
